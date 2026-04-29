4fa89224051abf6ca8622c25c3aded8154bcadeb
Denilson-S <denisilvalima536.com@gmail.com>
2026-04-29 08:23:38 -0300

    fix(): Corrige o CORS

--- api/index.js
+++ api/index.js
@@ -4,12 +4,14 @@ require('dotenv').config({ path: path.join(__dirname, '../.env') });
 process.env.TZ = process.env.TZ || 'UTC';
 
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 app.use(express.json());
 
 app.use('/api', routes);
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
--- app_quizz/lib/constants/app_defines.dart
+++ app_quizz/lib/constants/app_defines.dart
@@ -3,5 +3,5 @@ import 'package:dotenv/dotenv.dart';
 final env = DotEnv()..load();
 
 class AppDefines {
-  static String get apiUrl => env['API_URL'] ?? 'http://192.168.0.104:5000/api';
+  static String get apiUrl => env['API_URL'] ?? 'http://192.168.49.61:5000/api';
 }
